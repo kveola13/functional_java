3e2958564de2e738175ce23f966895765b9f988e
olakv <kveola13@student.westerdals.no>
2020-06-25 10:38:10 +0200

    added a better function

--- src/main/java/functionalinterface/_Function.java
+++ src/main/java/functionalinterface/_Function.java
@@ -1,10 +1,14 @@
 package functionalinterface;
 
+import java.util.function.Function;
+
 public class _Function {
     public static void main(String[] args) {
-        int increment = incrementByOne(0);
+        int increment = incrementByOne(1);
         System.out.println(increment);
     }
+
+    Function<Integer, Integer> incrementByOneFunction = number -> number++;
 
     static int incrementByOne(int number) {
         return number + 1;
