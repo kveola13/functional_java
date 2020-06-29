3b5c3f0b820dc6baea3798119e71f9e54438b581
olakv <kveola13@student.westerdals.no>
2020-06-29 20:58:00 +0200

    added a supplier

--- src/main/java/functionalinterface/_Supplier.java
+++ src/main/java/functionalinterface/_Supplier.java
@@ -2,10 +2,10 @@ package functionalinterface;
 
 public class _Supplier {
     public static void main(String[] args) {
-
+        System.out.println(getDBConnectionURL());
     }
 
     static String getDBConnectionURL() {
-        return "jdbc://localhost:3030";
+        return "jdbc://localhost:3030/users";
     }
 }
