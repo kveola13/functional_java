c6bccff4336961c0a83fc1426865ed0e3747161a
olakv <kveola13@student.westerdals.no>
2020-07-06 08:14:37 +0200

    Optionals

--- src/main/java/optionals/Main.java
+++ src/main/java/optionals/Main.java
@@ -4,7 +4,6 @@ import java.util.Optional;
 
 public class Main {
     public static void main(String[] args) {
-        Object object = Optional.ofNullable("Hello").orElseThrow(() -> new IllegalStateException("No can do"));
-        System.out.println(object);
+        Optional.ofNullable("Hello").ifPresent(System.out::println);
     }
 }
