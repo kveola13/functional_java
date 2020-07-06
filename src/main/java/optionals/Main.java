6b78449b6240d8b8210a48de13529ad209bbf601
olakv <kveola13@student.westerdals.no>
2020-07-06 08:17:13 +0200

    Optionals

--- src/main/java/optionals/Main.java
+++ src/main/java/optionals/Main.java
@@ -4,6 +4,9 @@ import java.util.Optional;
 
 public class Main {
     public static void main(String[] args) {
-        Optional.ofNullable("John@Wick.com").ifPresent(email -> System.out.println("Sending email to " + email));
+        Optional.ofNullable(null).ifPresentOrElse(email -> System.out.println("Sending email to " + email),
+                () -> {
+                    System.out.println("Can't send email to the Baba Yaega");
+                });
     }
 }
