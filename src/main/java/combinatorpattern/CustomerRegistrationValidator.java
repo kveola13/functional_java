51a9e78c2ef75712c244f191cc0af393d5d94b3b
olakv <kveola13@student.westerdals.no>
2020-07-08 10:20:09 +0200

    Customer registration validation

--- src/main/java/combinatorpattern/CustomerRegistrationValidator.java
+++ src/main/java/combinatorpattern/CustomerRegistrationValidator.java
@@ -1,11 +1,27 @@
 package combinatorpattern;
 
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.function.Function;
 
 import static combinatorpattern.CustomerRegistrationValidator.*;
+import static combinatorpattern.CustomerRegistrationValidator.ValidationResult.*;
 
-public interface CustomerRegistrationValidator extends Function <Customer, ValidationResult> {
-    enum ValidationResult{
+public interface CustomerRegistrationValidator extends Function<Customer, ValidationResult> {
+
+    static CustomerRegistrationValidator isEmailValid() {
+        return customer -> customer.getEmail().contains("@") ? SUCCESS : EMAIL_NOT_VALID;
+    }
+
+    static CustomerRegistrationValidator isPhoneNumberValid() {
+        return customer -> customer.getPhoneNumber().startsWith("+47") ? SUCCESS : PHONE_NUMBER_NOT_VALID;
+    }
+
+    static CustomerRegistrationValidator isAdult() {
+        return customer -> Period.between(customer.getDob(), LocalDate.now()).getYears() > 18 ? SUCCESS : IS_NOT_AN_ADULT;
+    }
+
+    enum ValidationResult {
         SUCCESS,
         PHONE_NUMBER_NOT_VALID,
         EMAIL_NOT_VALID,
