7eca9c8e589619b112278499cfbf11f497fd32b1
olakv <kveola13@student.westerdals.no>
2020-07-08 10:24:49 +0200

    Customer registration validation

--- src/main/java/combinatorpattern/Main.java
+++ src/main/java/combinatorpattern/Main.java
@@ -2,6 +2,9 @@ package combinatorpattern;
 
 import java.time.LocalDate;
 
+import static combinatorpattern.CustomerRegistrationValidator.*;
+import static combinatorpattern.CustomerRegistrationValidator.ValidationResult.*;
+
 public class Main {
     public static void main(String[] args) {
         Customer customer = new Customer(
@@ -13,5 +16,12 @@ public class Main {
 
         CustomerValidatorService service = new CustomerValidatorService();
         System.out.println(service.isValidCustomer(customer));
+
+        ValidationResult result = isEmailValid().and(isPhoneNumberValid()).and(isAdult()).apply(customer);
+        System.out.println(result);
+
+        if (result != SUCCESS) {
+            throw new IllegalStateException(result.name());
+        }
     }
 }
