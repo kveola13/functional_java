1717190c5b775ff22fd1ea90f1d8a6c12d3d99b5
olakv <kveola13@student.westerdals.no>
2020-07-09 09:12:34 +0200

    added Lambdas and changed language level

---
+++ src/main/java/js_section/Lambdas.java
@@ -0,0 +1,12 @@
+package js_section;
+
+import java.util.function.Function;
+
+public class Lambdas {
+    public static void main(String[] args) {
+        Function<String, String> upperCaseName = name -> {
+            if (name.isBlank()) throw new IllegalArgumentException("");
+            return name.toUpperCase();
+        };
+    }
+}
